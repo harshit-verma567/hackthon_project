89c8e8cb7af0ca5c2dab9b715b3c959c01f7f909
Harshit Verma <vharshit567@gmail.com>
2026-02-26 00:13:55 +0530

    Added backend

---
+++ Task Management App/Task-Management-App/src/main/java/com/hackathon/tma/Task/Management/App/TaskManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.tma.Task.Management.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagementAppApplication.class, args);
+	}
+
+}
